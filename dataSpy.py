1bdfe6ff343071653823be1a0a3df6b8e74b5225
Kurisu Cat <64016189+kurisucat@users.noreply.github.com>
2022-08-13 18:56:13 +0800

    🥴 同步 EOE.LOL 网站代码 修改时间算法

--- dataSpy.py
+++ dataSpy.py
@@ -14,7 +14,7 @@ def wait(): #“等等陈睿”函数
         太快会被陈睿 Gank，特此写函数。
     """
 
-    sec = 0.88 #这里填等待秒 太快会被 -412 拦截
+    sec = 2.33 #这里填等待秒 太快会被 -412 拦截
     time.sleep(sec)
 
 def videoSearch(kw, pg, printOrNot=True): #视频搜索函数
@@ -54,26 +54,31 @@ def dataSpy(kw, pg): #数据捉虫函数
         __filter = False        #初始化 过滤
         __weight = 0            #初始化 权重值
         __authorExclude = False #初始化 排除
+        __debug = ""            #初始化 测试变量
         for b in i:           # 遍历  字典
             if b == "title":  # 消除  <em> 关键词标记
                 i[b] = i[b].replace('<em class="keyword">', "").replace('</em>', "")
-                print("标题：", i[b])
+                #print("标题：", i[b])
+                __debug = __debug+"标题:"+str(i[b])+"。"
             if b == "pic":  # 加載縮略圖照片
                 i[b] = i[b]+"@544w_340h_1c"
             if b == "author": #判定 作者
                 for BlackAuthor in authorBlackList:
                     if i[b].find(BlackAuthor) != -1:
-                        print("已排除并 __authorExclude 设置为真：", i[b])
+                        #print("已排除并 __authorExclude 设置为真：", i[b])
                         __weight_author = -9765
                         __authorExclude = True
+                        __debug = __debug+"作者已排除。"
                     else:
                         pass
             if b == "like":   #权重 计算点顶
                 __like = i[b]
                 if i[b] <= 900:
                     __weight_like = i[b] * 1.21
+                    __debug = __debug+"低顶率。"
                 else:
                     __weight_like = i[b] * 0.18
+                    __debug = __debug+"高顶率。"
             if b == "coin":   #权重 计算投币 妈的个byd写半天发现返回的信息根本没有投币数
                 __weight_coin = 0
             #     if __like <= i[b]:
@@ -84,31 +89,42 @@ def dataSpy(kw, pg): #数据捉虫函数
             #         __weight_coin = i[b]
             if b == "favorites":    #权重 计算收藏
                 __weight_collect = i[b] * 0.3
-            if b == "senddate":     #权重 计算时效
-                __sendTimeCalc = int(int(time.time()) - int(i[b]))
-                if __sendTimeCalc >= 432000:  #老的视频 5*24*60*60秒后（五天后）
-                    __weight_sendTime = -345080
-                if __sendTimeCalc <= 259200:  #新的视频
-                    __weight_sendTime = 99999
+                __debug = __debug+"收藏:"+str(i[b])+"。"
+            if b == "pubdate":     #权重 计算时效
+                __sendTimeCalc = int(int(time.time()) - int(i[b]))/(60*60*24)
+                __debug = __debug+"时效:"+str(i[b])+"。"
+                if __sendTimeCalc >= 5:  #老的视频 5*24*60*60秒后（五天后）
+                    __weight_sendTime = -9345080
+                    __debug = __debug+"老视频。"
+                    #print("too old")
+                if __sendTimeCalc <= 2:  #新的视频
+                    #print("New video")
+                    __weight_sendTime = 48706
+                    __debug = __debug+"更新的视频。"
             if b == "play":         #权重 计算点阅
-                if i[b] <= 2000:
+                if i[b] <= 5000:
                     __weight_click = ( 12 + i[b] ) * 1.2
+                    __debug = __debug+"观看很少。"
                 elif i[b] <= 30000:
                     __weight_click = ( 9 + i[b] ) * 0.7
+                    __debug = __debug+"观看三万内。"
                 else:
-                    __weight_click = ( 1 + i[b] ) * 0.06
+                    __weight_click = ( 1 + i[b] ) * 0
+                    __debug = __debug+"观看过多。"
             if b == "video_review": #权重 计算弹幕
                 if i[b] <= 100:
-                    __weight_danmaku = i[b] * 0.5
+                    __weight_danmaku = i[b] * 0.2
+                __debug = __debug+"弹幕:"+str(i[b])+"。"
             if b == "hit_columns":
                 if i[b] == ["author"]:
                     __filter = True
             
         #↓↓↓↓ 计算权重
         __weight = int(__weight_random + __weight_click + __weight_sendTime + __weight_author + __weight_danmaku + __weight_collect + __weight_coin + __weight_like)
-        print("权重值：", __weight)
+        #print("权重值：", __weight)
         # 在视频结果列表中插入权重
-        i.update({"__weight": __weight, "__filter": __filter, "__authorExclude": __authorExclude})
+        __debug = ""
+        i.update({"__weight": __weight, "__filter": __filter, "__authorExclude": __authorExclude, "debug": __debug})
         i = videoResult                  #将缓存中的列表赋给结果 并返回
     return videoResult
 
@@ -150,8 +166,8 @@ def makeJson(): #制作词典列表函数
     BlockWord = ["多米诺", "凇子M", "黑猫与白喵", "米诺地尔", "明日方舟早露", "明日方舟", "舒舒酷北北", "贤宝宝Baby", "多米诺骨牌", "微物米诺", "天天打龟", "六弦阁徒_HTT", "街头社区", "艾森巴赫", "撒旦女巫的诱惑", "锤子game", "三千亿光年", "不知所措的周余", "十一点睡粥老师", "少喝运动多奶茶", "tsuiruaku", "战舰世界", "元首的渣渣", "菲尔米诺Bobby", "青衣之冇", "账号注销9999000", "非那米诺", "梦中の游乐园", "坦克世界"]
     #↑↑↑↑无关结果关键词 用于排除
     #print("列表长度：",len(result))
-    for times in range(0,50): #反复循环过滤 50 遍无关视频 不知道为什么但是确实很有效果
-        print("\n列表长度：", len(result))
+    for times in range(0,10): #反复循环过滤 50 遍无关视频 不知道为什么但是确实很有效果
+        #print("\n列表长度：", len(result))
         for bL in BlockWord:  #遍历无关视频关键词列表
             for i in result:  #对 标题、作者、简介、标签 检查关键词
                 if i['title'].find(bL) != -1:
@@ -179,7 +195,7 @@ def makeJson(): #制作词典列表函数
                         result.remove(i)
                     except ValueError:
                         pass
-    print("\n\n运行完毕，列表长度：", len(result))
+    print("\n\n列表长度：", len(result))
     return result
 
 result = makeJson()
